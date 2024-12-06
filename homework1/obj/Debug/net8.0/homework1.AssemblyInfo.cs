f2aa7d9ec6a8af5e84395a0503c6e95809021a12
djPlesca36 <alexandru.plesca@ulbsibiu.ro>
2024-12-06 08:24:07 +0200

    2

--- homework1/obj/Debug/net8.0/homework1.AssemblyInfo.cs
+++ homework1/obj/Debug/net8.0/homework1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("homework1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d86732566d55d013849b48db137b377be38ed320")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6e2497fa63481f7146bb93756235beb471544c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("homework1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("homework1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
